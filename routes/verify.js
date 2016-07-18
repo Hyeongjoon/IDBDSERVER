ac39f2c27b9ab17f3afd1f580df4be47f7d133ae
Hyeong joon Jang <Hyeong joon Jang>
2016-07-18 12:34:09 +0900

    16-07-18

--- routes/verify.js
+++ routes/verify.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var async = require('async');
 var DecryptHelper = require('../helper/DecryptHelper');
+var UserDAO = require('../model/UserDAO');
 
 router.get('/' , function(req , res , next){
 	if(req.url.length <= 8){
@@ -12,9 +13,11 @@ router.get('/' , function(req , res , next){
 			console.log(tmp);
 			var tmpEmail = tmp.slice(8);
 			var email = DecryptHelper.decryptEmail(tmpEmail);
-			console.log(email);
-		}], 
-		function(err , results){
+			UserDAO.findUserByEmail(email , callback);
+		}, function(args1 , callback){
+			console.log(args1);
+			
+		}], function(err , results){
 			
 		});
 	}
