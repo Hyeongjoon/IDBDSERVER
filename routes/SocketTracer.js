d321d12db56298a46ad6520b2e619e0f56057405
Hyeong joon Jang <Hyeong joon Jang>
2016-07-19 12:07:27 +0900

    16-07-19

--- routes/SocketTracer.js
+++ routes/SocketTracer.js
@@ -28,7 +28,7 @@ io.on('connection', function(socket) {
 				return;
 			} else{
 				if(decryptHelper.decryption(result[0][0].password)== data.password){
-					if(result[0][0].phone_verify==true){
+					if(result[0].phone_verify==true){
 					var result = {result : "true"};
 					socket.emit('login_result' , result);
 					socket.handshake.session.login = true;
