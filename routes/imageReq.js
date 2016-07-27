63f557838e0232fe34c75f1dfb32b0549e8674b1
Hyeong joon Jang <Hyeong joon Jang>
2016-07-27 16:14:39 +0900

    16-07-27

--- routes/imageReq.js
+++ routes/imageReq.js
@@ -9,10 +9,8 @@ AWS.config.region = 'ap-northeast-2';
 router.get('/' , function(req ,res ,next){
 	var s3 = new AWS.S3();
 	var params = config.awsS3GetConfig;
-	var file = require('fs').createWriteStream('./temp/temp444.png');
-	s3.getObject(params , function(err , data){
-		console.log(err);
-		console.log(data);
+	s3.getSignedUrl('getObject', params, function (err, url) {
+		  console.log("The URL is", url);
 	});
 });
 
