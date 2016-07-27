8815b8b0c2819e854a5c14b339c02aef1efcdc55
Hyeong joon Jang <Hyeong joon Jang>
2016-07-27 16:02:09 +0900

    16-07-27

--- routes/imageReq.js
+++ routes/imageReq.js
@@ -5,11 +5,11 @@ var AWS = require('aws-sdk');
 AWS.config.region = 'ap-northeast-2';
 
 router.get('/' , function(req ,res ,next){
-	console.log("여긴오냐?");
 	var s3 = new AWS.S3();
-	var params = {Bucket : 'sendwitchtracer' , Key : 'temp.png'};
-	var file = require('fs').createWriteStream('./temp/temp.png');
-	s3.getObject(params).createReadStream().pipe(file);	
+	var params = {Bucket : 'sendwitchtracer' , Key : 'profile/temp.png'};
+	var file = require('fs').createWriteStream('./temp/temp1.png');
+	s3.getObject(params).createReadStream().pipe(file);
+	
 });
 
 
