eb7eea006687d02db9204f12243f4b7efa9f7655
Hyeong joon Jang <Hyeong joon Jang>
2016-07-27 16:09:56 +0900

    16-07-27

--- routes/imageReq.js
+++ routes/imageReq.js
@@ -10,8 +10,10 @@ router.get('/' , function(req ,res ,next){
 	var s3 = new AWS.S3();
 	var params = config.awsS3GetConfig;
 	var file = require('fs').createWriteStream('./temp/temp444.png');
-	s3.getObject(params).createReadStream().pipe(file);
-	
+	s3.getObject(params , function(err , data){
+		console.log(err);
+		console.log(data);
+	});
 });
 
 
