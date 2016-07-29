e1825e6491c8ee7da83100e3adf2e9f34fc8ac99
Hyeong joon Jang <Hyeong joon Jang>
2016-07-29 17:39:19 +0900

    16-07-29

--- model/Belong_grDAO.js
+++ model/Belong_grDAO.js
@@ -7,11 +7,11 @@ exports.getGidByUid = function(uid , callback){
 };
 
 exports.getUidInGroup = function(gidArr , callback){
-	var sqlQuery = 'Select * from belong_gr WHERE gid = ';
+	var sqlQuery = 'Select * from belong_gr WHERE (gid = ';
 	for (var i = 0 ; i < gidArr.length ; i ++){
 		sqlQuery = sqlQuery + gidArr[i].gid + ','
 	}
 	sqlQuery = sqlQuery.substring(0,sqlQuery.length-1);
-	sqlQuery = sqlQuery + ' AND  (uid != ' + mysql.escape(gidArr[0].uid) + ')';
+	sqlQuery = sqlQuery + ') AND  (uid != ' + mysql.escape(gidArr[0].uid) + ')';
 	base.select(sqlQuery , callback);
 };
