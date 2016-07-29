155c8229959e9f1e7691e9404b0d143cdb7a7140
Hyeong joon Jang <Hyeong joon Jang>
2016-07-29 17:13:44 +0900

    16-07-29

--- routes/SocketTracer.js
+++ routes/SocketTracer.js
@@ -97,10 +97,10 @@ io.on('connection', function(socket) {
 	socket.on('getGroupImage', function(data){
 		async.waterfall([function(callback){
 			userDAO.findUserByEmail(socket.handshake.session.email , callback);
-		}, function(args1 , callback){ 
+		}, function(args1, callback){ 
 			belong_grDAO.getGidByUid(args1[0].uid , callback);
-		},function(args1 , callback){
-			console.log(args1);
+		},function(args1, callback){
+			console.log(args1.length);
 		}, function(args1 , callback){
 			var params = config.awsS3GetConfig;
 			params.Key = args1[0].profile;
