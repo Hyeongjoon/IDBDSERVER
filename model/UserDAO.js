8ac888bdad2e09f9f80c1e55e7c7a68a48b3d705
Hyeong joon Jang <Hyeong joon Jang>
2016-08-09 13:11:10 +0900

    16-08-09

--- model/UserDAO.js
+++ model/UserDAO.js
@@ -41,7 +41,7 @@ exports.getUserNameByUID = function( uidArr , callback ){
 		}
 	sqlQuery = sqlQuery.substring(0 , sqlQuery.length-4);
 	sqlQuery = sqlQuery+';'
-	console.log(sqlQuery);
+	base.select(sqlQuery , callback);
 }
 
 /*
