bfa099b0ad1bad518e151f5a213e1fc6a80b68fd
Hyeong joon Jang <Hyeong joon Jang>
2016-08-12 12:22:02 +0900

    16-08-12

--- helper/AlramHelper.js
+++ helper/AlramHelper.js
@@ -1,3 +1,7 @@
 exports.classifyAlram = function(args){
-	console.log(args);
+	var kind = {0: '' , 1 : '' , 2: '' , 3:'' , 4:'' , 5:'' , 6:'' };
+	for(var i = 0 ; i < args.length ; i++){
+		
+		
+	}
 };
--- model/AlramDAO.js
+++ model/AlramDAO.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 
 exports.findAlramByUid = function(uid , callback){
 	
-	sqlQuery = 'SELECT * from alram where unconfirmed = ' + mysql.escape(true) + ' AND target_uid = ' + mysql.escape(uid) + ' ORDER BY updated_time desc';
+	sqlQuery = 'SELECT * from alram where unconfirmed = ' + mysql.escape(true) + ' AND target_uid = ' + mysql.escape(uid) + ' ORDER BY updated_time desc Column Limit 0,50';
 	
 	base.select(sqlQuery , callback);
 }
