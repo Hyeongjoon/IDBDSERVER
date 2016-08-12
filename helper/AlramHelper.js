ce7b6ee79c380ee890fa2d17ca7fd1790a9282fc
Hyeong joon Jang <Hyeong joon Jang>
2016-08-12 12:27:59 +0900

    16-08-12

--- helper/AlramHelper.js
+++ helper/AlramHelper.js
@@ -1,5 +1,5 @@
 exports.classifyAlram = function(args){
-	var kind = {0: '' , 1 : '' , 2: '' , 3:'' , 4:'' , 5:'' , 6:'' };
+	var kind = {0: [] , 1 : [] , 2: [] , 3: [] , 4: [] , 5: [] , 6: [] };
 	for(var i = 0 ; i < args.length ; i++){
 		
 		
