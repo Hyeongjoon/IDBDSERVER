1e3ebc2be89bc857d2fd6f68c09e93c421758f09
Hyeong joon Jang <Hyeong joon Jang>
2016-08-12 12:25:54 +0900

    16-08-12

--- model/AlramDAO.js
+++ model/AlramDAO.js
@@ -3,7 +3,7 @@ var mysql = require('mysql');
 
 exports.findAlramByUid = function(uid , callback){
 	
-	sqlQuery = 'SELECT * from alram where unconfirmed = ' + mysql.escape(true) + ' AND target_uid = ' + mysql.escape(uid) + ' ORDER BY updated_time desc WHERE row_count() < 50';
+	sqlQuery = 'SELECT * from alram where unconfirmed = ' + mysql.escape(true) + ' AND row_count() < 50 AND target_uid = ' + mysql.escape(uid) + ' ORDER BY updated_time desc';
 	
 	base.select(sqlQuery , callback);
 }
