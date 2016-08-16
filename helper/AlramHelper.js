d9d4ed6cb2631b16db5486d5e58c5ab9b194e7d6
Hyeong joon Jang <Hyeong joon Jang>
2016-08-16 14:08:37 +0900

    16-08-16

--- helper/AlramHelper.js
+++ helper/AlramHelper.js
@@ -43,7 +43,23 @@ exports.addProfileUID  = function(addTarget , contentsArr , callback){
 	
 	for(var i = 0 ; i < addTarget.reply.length ; i++){
 		addTarget.reply[i].alramProfileUid = contentsArr[3][i].writer;
-		addTarget.reply[i].replyText = contentsArr[3][i].contents.substring(0,20);
+		var temp = contentsArr[3][i].contents.substring(0,10) +'...';
+		addTarget.reply[i].replyText = temp;
 	}//reply 정보처리
+	
+	for(var i = 0 ; i < addTarget.re_reply.length ; i++){
+		addTarget.re_reply[i].alramProfileUid = contentsArr[4][i].writer;
+		var temp = contentsArr[4][i].contents.substring(0,10) +'...';
+		addTarget.re_reply[i].re_replyText = temp;
+	}//re_reply 정보처리
+	
+	for(var i = 0 ; i < addTarget.like_reply.length ; i++){
+		addTarget.like_reply[i].alramProfileUid = contentsArr[5][i]. belong_uid;
+	}//like_reply 정보처리
+	
+	for(var i = 0 ; i < addTarget.like_re_reply.length ; i++){
+		addTarget.dislike[i].alramProfileUid = contentsArr[6][i]. belong_uid;
+	}//like_re_reply 정보처리
+	
 	console.log(addTarget);
 }
