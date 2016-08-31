43a2333ff5bb4f757207556a7afa5e162d641781
Hyeong joon Jang <Hyeong joon Jang>
2016-08-31 14:21:23 +0900

    16-08-31

--- model/LocationDAO.js
+++ model/LocationDAO.js
@@ -5,7 +5,7 @@ exports.findRecentLocationByGid = function(gidArr , callback){
 	var sqlQuery = 'select * from location where (belonged_gid, updated_time) in' + 
 		'(select belonged_gid,max(updated_time) from location WHERE belonged_gid =';
 	for (var i = 0 ; i < gidArr.length ; i++){
-		sqlQuery = sqlQuery + gidArr[i].gid + ' OR belonged_giod = ';
+		sqlQuery = sqlQuery + gidArr[i].gid + ' OR belonged_gid = ';
 	}
 	
 	sqlQuery = sqlQuery.substring(0 , sqlQuery.length-19);
