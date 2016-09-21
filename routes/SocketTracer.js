bdf4e9765d76c78bee52ab7e5ffe3ee4c73ad521
Hyeong joon Jang <Hyeong joon Jang>
2016-09-21 23:33:28 +0900

    '16-09-21

--- routes/SocketTracer.js
+++ routes/SocketTracer.js
@@ -364,7 +364,8 @@ io.on('connection', function(socket) {
 			} , function(args1 , callback){
 				groupDAO.addGroupReturnID(callback);
 			} , function(args1 , callback){
-				belong_grDAO.addBelong_gr(socket.handshake.session.uid , args1 , data , callback);
+				console.log(args1);
+				//belong_grDAO.addBelong_gr(socket.handshake.session.uid , args1 , data , callback);
 			}] , function(err , results){
 				console.log(results);
 			});
