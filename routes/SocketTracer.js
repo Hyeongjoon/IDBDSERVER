20edbff3f30cca896a2f074304fbbb5586aaf4ac
Hyeong joon Jang <Hyeong joon Jang>
2016-09-23 17:05:21 +0900

    16-09-23

--- routes/SocketTracer.js
+++ routes/SocketTracer.js
@@ -385,21 +385,21 @@ io.on('connection', function(socket) {
 	});
 	
 	socket.on('deleteGroup' , function(data){
-		async.waterfall([function(callback){
+		async.waterfall([function(callback) {
 			console.log(data);
 			belong_grDAO.deleteBelong_gr(socket.handshake.session.uid , data[0] , callback);
-		} , function(args1 , callback){
+		} , function(args1 , callback) {
 			groupDAO.findGrInfrom(data[0] , callback);
-		} , function(args1 , callback){
-			if(args1.length==0){
+		} , function(args1 , callback) {
+			if (args1.length==0) {
 				console.log("델레트할 그룹이 없다는데 없으면 안되는데...");
-			} else if(args1[0].member_number==1){
+			} else if (args1[0].member_number==1) {
 				groupDAO.deleteGroup(data[0] , callback);
-			} else{
+			} else {
 				groupDAO.subtractGroupNum(data[0] , callback);
 			}
-		} , function(args1 , callback){
-				belong_grDAO
+		} , function(args1 , callback) {
+			
 		}] , function(err , results){
 			
 		});
