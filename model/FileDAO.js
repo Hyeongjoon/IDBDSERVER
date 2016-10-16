d14c8497d15435f3f6e95041fa28123a756ac4d7
Hyeong joon Jang <Hyeong joon Jang>
2016-10-17 01:03:58 +0900

    16-10-17

--- model/FileDAO.js
+++ model/FileDAO.js
@@ -2,6 +2,6 @@ var base = require('./BaseDAO.js');
 var mysql = require('mysql');
 
 exports.findFileByDate = function ( gid , callback){
-	var sqlQuery = 'SELECT * FROM file_table group by date(upload_time) WHERE gid = ' + mysql.escape(gid);
+	var sqlQuery = 'SELECT * FROM file_table WHERE gid = ' + mysql.escape(gid) + ' group by date(upload_time)';
 	base.select(sqlQuery , callback);
 }
