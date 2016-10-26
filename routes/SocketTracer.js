49416d87c78b1360c32cd678793723326435492e
Hyeong joon Jang <Hyeong joon Jang>
2016-10-27 03:46:33 +0900

    16-10-27

--- routes/SocketTracer.js
+++ routes/SocketTracer.js
@@ -591,8 +591,8 @@ io.on('connection', function(socket) {
 					var temp = results[0].location.split('/');  //파일 이름 설정하는곳
 					results[0].file_name = temp[2].substr(17); //나중에 파일 시스템 완료 되면 타임스탬프로 이름 찍을꺼니까 그때 다시 바꿀것
 					results[0].location = url;
+					socket.emit('addFileResult' , results[0]);
 				});
-				socket.emit('addFileResult' , results[0]);
 			}else {
 				//여기가 에러처리지 뭐하는샛끼야진짜.......허.........
 			}
